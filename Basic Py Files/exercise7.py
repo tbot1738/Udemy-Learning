24482be098971dc0ed05873c1abc7fb303d5f636
tbot <tbot1738@gmail.com>
2022-09-29 18:33:31 +0530

    added direcotry for basic py files

--- Basic Py Files/exercise7.py
+++ Basic Py Files/exercise7.py
@@ -1,8 +1,4 @@
-from random import randint
-# generate a number 1~10
-
-# input from user?
-# check that input is a number 1~10
+import random
 
 
 def game_guess(guess, answer):
@@ -12,14 +8,15 @@ def game_guess(guess, answer):
             return True
     else:
         print('hey bozo, I said 1~10')
+        return False
 
 
-if __name__ == "__main__":
-    answer = randint(1, 10)
+if __name__ == '__main__':
+    answer = random.randint(1, 10)
     while True:
         try:
             guess = int(input('guess a number 1~10:  '))
-            if game_guess(guess, answer):
+            if (game_guess(guess, answer)):
                 break
         except ValueError:
             print('please enter a number')
