a861e740923fcfc0ce6c41c7d3564144dc7edc79
tbot <tbot1738@gmail.com>
2022-09-30 10:00:43 +0530

    Basic Email Sending Project(postponed)

---
+++ Projects/Email Playground/email_sender.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Tbot'
+email['to'] = 'technokrish1780@gmail.com'
+email['subject'] = 'Six mahine ruk ja meri body banane waali hai'
+
+email.set_content("I am python master boii!!")
+
+with smtplib.SMTP("smtp.gmail.com",587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login("tbot1738@gmail.com", "TBOt1738")
+    smtp.send_message(email)
+    print("All good bozo!")
